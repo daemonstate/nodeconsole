b9e58b4590091ff37c3d0ec22ae8a82f2b5ca643
daemonstate <daemon.state@gmail.com>
2022-06-17 23:33:31 +0600

    added json as a route

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ console.log("Hello World");
 app.get('/', (req, res) => {
   absolutePath = __dirname+"/views/index.html";
   res.sendFile(absolutePath);
+  
+});
+app.get('/json',(req,res)=>{
+  res.json({"message": "Hello json"});
 });
 pathforcss = __dirname+'/public';
 app.use("/public",express.static(pathforcss));
