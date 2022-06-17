7ac3dd06c1b04b04cbea2f9f018f310eb916de35
daemonstate <daemon.state@gmail.com>
2022-06-17 23:27:42 +0600

    added css

--- myApp.js
+++ myApp.js
@@ -4,7 +4,9 @@ console.log("Hello World");
 app.get('/', (req, res) => {
   absolutePath = __dirname+"/views/index.html";
   res.sendFile(absolutePath);
-})
+});
+pathforcss = __dirname+'/public';
+app.use("/public",express.static(pathforcss));
 
 
 
