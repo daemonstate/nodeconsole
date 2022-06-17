90a82d79ce31e87ad45051b44487fdd76a3d43e3
daemonstate <daemon.state@gmail.com>
2022-06-17 23:15:54 +0600

    added sendfile

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.get('/', (req, res) => {
-  absolutePath = __dirname+/views/index.html;
+  absolutePath = __dirname+"/views/index.html";
   res.sendFile(absolutePath);
 })
 
