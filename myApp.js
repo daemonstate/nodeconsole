500328a41048025e34ec5fe08495c4e9d0047e0b
daemonstate <daemon.state@gmail.com>
2022-06-21 00:26:36 +0600

    parsed POST request

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 require('dotenv').config();
+let bodyParser = require('body-parser');
 let app = express();
 console.log("Hello World");
+app.use(bodyParser.urlencoded({extended: false}));
 app.use((req,res,next)=>{
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
